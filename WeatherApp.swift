2f9860998fc93ca00e0c8ee2a9f81fb3f6ef69da
ichi <realkouu@gmail.com>
2026-02-04 14:11:30 +0900

    Initial commit

---
+++ WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 福田光一郎 on 2026/02/04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
